63e35544b2cb246620ed9cac1c17c32cd297332c
TheerthaPraveen <theerthapraveen.n@gmail.com>
2022-07-08 12:46:05 +0530

    gallery-viewed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GalleryViewComponent } from './gallery-view/gallery-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GalleryViewComponent
   ],
   imports: [
     BrowserModule,
